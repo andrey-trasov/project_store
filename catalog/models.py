fc36abfa8f45d4f682d1d007268ae1f4d5fe25d8
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2024-06-06 18:18:14 +0300

    20.1 4

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,36 @@
+import datetime
 from django.db import models
+from django.db.models import IntegerField
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование товара')
+    description = models.TextField (verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование', help_text='Введите наименование товара')
+    description = models.TextField (verbose_name='Описание')
+    image = models.ImageField(upload_to='product/photo', verbose_name='Изображение', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = IntegerField(verbose_name='Цена ')
+    created_at = models.DateField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateField(auto_now=True, verbose_name='Дата последнего изменения')
+    manufactured_at = models.DateField(verbose_name='Дата производства продукта', default=datetime.date.today)
+
+    class Meta:
+        verbose_name = 'пробукт'
+        verbose_name_plural = 'пробукты'
+
+    def __str__(self):
+        return self.name
+
+
+
--- myproject/settings.py
+++ myproject/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 
@@ -51,19 +52,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'store',
+        'USER':  'postgres',
+        'PASSWORD': '12345',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -81,8 +80,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
 
@@ -93,15 +90,15 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
 
 STATICFILES_DIRS = (BASE_DIR / 'static',)
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')

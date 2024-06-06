165e109b79d4e07708f82d5b58ba716d064e958a
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2024-06-06 23:39:26 +0300

    20.1

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -2,6 +2,7 @@ from django.core.management import BaseCommand
 from catalog.models import Category, Product
 import json
 
+
 class Command(BaseCommand):
 
     @staticmethod
@@ -23,37 +24,37 @@ class Command(BaseCommand):
         # Удалите все категории
         Product.objects.all().delete()
         # Создайте списки для хранения объектов
-        product_for_create = []
-        category_for_create = []
+        product_list = []
+        category_list = []
 
-		# Обходим все значения категорий из фиктсуры для получения информации об одном объекте
+        # Обходим все значения категорий из фиктсуры для получения информации об одном объекте
         for category in Command.json_read_categories():
-            category_for_create.append(
+            category_list.append(
                 {"id": category['pk'], "name": category['fields']['name'],
                  "description": category['fields']['description']}
             )
         category_for_create = []
-        for category_item in category_for_create:
+        for category_item in category_list:
             category_for_create.append(
                 Category.objects.create(**category_item)
             )
 
         # Обходим все значения продуктов из фиктсуры для получения информации об одном объекте
         for product in Command.json_read_products():
-            product_for_create.append(
+            product_list.append(
                 {"id": product['pk'], "name": product['fields']['name'],
                  "description": product['fields']['description'],
                  "price": product['fields']['price'],
-                 "category": Product.objects.get(pk=product['fields']['category']),
+                 "category": Category.objects.get(pk=product['fields']['category']),
                  "image": product['fields']['image'], "created_at": product['fields']['created_at'],
                  "updated_at": product['fields']['updated_at']}
             )
         product_for_create = []
-        for product_item in product_for_create:
+        for product_item in product_list:
             product_for_create.append(
                 Product.objects.create(**product_item)
             )
 
-				# Создаем объекты в базе с помощью метода bulk_create()
+        # Создаем объекты в базе с помощью метода bulk_create()
         Category.objects.bulk_create(category_for_create)
         Product.objects.bulk_create(product_for_create)

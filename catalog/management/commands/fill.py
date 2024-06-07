346ea62a6f3a18fc254af676800ff5fce08fc587
andrey-trasov <159887936+andrey-trasov@users.noreply.github.com>
2024-06-07 19:17:56 +0300

    20.1

--- catalog/management/commands/fill.py
+++ catalog/management/commands/fill.py
@@ -37,8 +37,10 @@ class Command(BaseCommand):
         category_for_create = []
         for category_item in category_list:
             category_for_create.append(
-                Category.objects.create(**category_item)
+                Category(**category_item)
             )
+
+        Category.objects.bulk_create(category_for_create)
 
         # Обходим все значения продуктов из фиктсуры для получения информации об одном объекте
         for product in Command.json_read_products():
@@ -57,7 +59,5 @@ class Command(BaseCommand):
             )
 
         # Создаем объекты в базе с помощью метода bulk_create()
-        Category.objects.bulk_create(category_for_create)
+        # Category.objects.bulk_create(category_for_create)
         Product.objects.bulk_create(product_for_create)
-
-#                Product.objects.create(**product_item)
